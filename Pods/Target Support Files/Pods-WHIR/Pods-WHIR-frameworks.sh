eab7b29f90df32e3aa38faf2083e0136d6852241
Bruce <t15thbruce@outlook.de>
2019-02-08 15:46:45 -0800

    save

--- Pods/Target Support Files/Pods-WHIR/Pods-WHIR-frameworks.sh
+++ Pods/Target Support Files/Pods-WHIR/Pods-WHIR-frameworks.sh
@@ -144,10 +144,12 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/KMPlaceholderTextView/KMPlaceholderTextView.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Seam3/Seam3.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Sentry/Sentry.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/KMPlaceholderTextView/KMPlaceholderTextView.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Seam3/Seam3.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Sentry/Sentry.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
--- WHIR/CoreData/CoreDataStack.swift
+++ WHIR/CoreData/CoreDataStack.swift
@@ -9,24 +9,125 @@
 import Foundation
 import CoreData
 import UIKit
+import Seam3
 
 class CoreDataStack {
 
+    var smStore: SMStore?
+    
     lazy var managedObjectContext: NSManagedObjectContext = {
         let container = self.persistentContainer
         return container.viewContext
     }()
 
-    private lazy var persistentContainer: NSPersistentContainer = {
-        let container = NSPersistentContainer(name: "WHIR")
-        container.loadPersistentStores { (_, error) in
-            if let error = error as NSError? {
-                // TODO: Find way to alert the user from here
-                print(error)
+    func migrateToSeamStore() {
+        
+        // store references to URLs necessary for migration
+        let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).last!
+        let oldUrl = documentDirectory.appendingPathComponent("WHIR.sqlite")
+        let newUrl = documentDirectory.appendingPathComponent("WHIR-Seam.sqlite")
+        let modelUrl = Bundle.main.url(forResource: "WHIR", withExtension: "momd")
+        
+        if let managedObjectModel = NSManagedObjectModel(contentsOf: modelUrl!) {
+            let container = NSPersistentContainer(name: "migrationContainer", managedObjectModel: managedObjectModel)
+            let coordinator = container.persistentStoreCoordinator
+            
+            guard let _ = try? coordinator.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: oldUrl, options: nil), let oldStore = coordinator.persistentStore(for: oldUrl) else {
+                fatalError("Failed to reference or create old store")
+            }
+            
+            do {
+                SMStore.syncAutomatically = false
+                try coordinator.migratePersistentStore(oldStore, to: newUrl, options:nil, withType:SMStore.type)
+                verifyCloudKitAuth()
+                self.smStore?.triggerSync()
+                SMStore.syncAutomatically = true
+            } catch {
+                fatalError("Failed to migrate store: \(error)")
             }
         }
-        return container
+        
+    }
+    
+    private lazy var persistentContainer: NSPersistentContainer = {
+        
+        SMStore.registerStoreClass()
+        
+        let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        
+        if let applicationDocumentsDirectory = urls.last {
+            
+            let url = applicationDocumentsDirectory.appendingPathComponent("WHIR-Seam.sqlite")
+            let seamStoreExists = FileManager.default.fileExists(atPath: url.path)
+            
+            if seamStoreExists == false {
+                migrateToSeamStore()
+            } else {
+                let container = NSPersistentContainer(name: "WHIR-Seam")
+                let coordinator = container.persistentStoreCoordinator
+                
+                let storeDescription = NSPersistentStoreDescription(url: url)
+                storeDescription.type = SMStore.type
+                container.persistentStoreDescriptions=[storeDescription]
+                
+                container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+                    if let error = error as NSError? {
+                        // Replace this implementation with code to handle the error appropriately.
+                        // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                        
+                        /*
+                         Typical reasons for an error here include:
+                         * The parent directory does not exist, cannot be created, or disallows writing.
+                         * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+                         * The device is out of space.
+                         * The store could not be migrated to the current model version.
+                         Check the error message to determine what the actual problem was.
+                         */
+                        fatalError("Unresolved error \(error), \(error.userInfo)")
+                    }
+                })
+                return container
+            }
+        }
+        
+        fatalError("Unable to access documents directory")
     }()
+    
+    func verifyCloudKitAuth() {
+        self.smStore?.verifyCloudKitConnectionAndUser() { (status, user, error) in
+            guard status == .available, error == nil else {
+                NSLog("Unable to verify CloudKit Connection \(error)")
+                return
+            }
+            
+            guard let currentUser = user else {
+                NSLog("No current CloudKit user")
+                return
+            }
+            
+            var completeSync = false
+            
+            let previousUser = UserDefaults.standard.string(forKey: "CloudKitUser")
+            if  previousUser != currentUser {
+                do {
+                    print("New user")
+                    try self.smStore?.resetBackingStore()
+                    completeSync = true
+                } catch {
+                    NSLog("Error resetting backing store - \(error.localizedDescription)")
+                    return
+                }
+            }
+            
+            UserDefaults.standard.set(currentUser, forKey:"CloudKitUser")
+            
+            self.smStore?.triggerSync(complete: completeSync)
+        }
+    }
+    
+    init() {
+        self.smStore = persistentContainer.persistentStoreCoordinator.persistentStores.first as? SMStore
+    }
 }
 
 //extension to saveChanges in managedObjectCOntext
